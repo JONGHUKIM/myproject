7bcc5f68e750fe98ed229e98f3110dd24cde1bbf
JongHuKim <hotsky803@gmail.com>
2024-11-25 14:12:03 +0900

    local2에서 커밋

--- myproject/src/com/itwill/myproject/ProjectMain.java
+++ myproject/src/com/itwill/myproject/ProjectMain.java
@@ -3,10 +3,10 @@ package com.itwill.myproject;
 public class ProjectMain {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Git!");
+		System.out.println("Hello asad");
 		System.out.println("안녕하세요, 깃허브!");
 		System.out.println("깃허브에서 작성된 ...");
-
+		
 	}
 
 }
